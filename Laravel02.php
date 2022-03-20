4ecae8ad251c413ed7caef3d00c28ebed9a7d5e8
1935Hiro <espresso.spacewood@gmail.com>
2022-03-20 06:54:54 +0000

    third commit

--- Laravel02.php
+++ Laravel02.php
@@ -1,2 +1,38 @@
 <? php
-    echo $hoge;
+    //1問目
+     $a = 3;
+     $b = 7;
+     echo $a+$b;
+    
+    //2問目
+    $array_month = array["January","February","March","April","May","June","July","August","September","October","November","December"]
+    echo array[7];
+    
+    //3問目
+    $hello = 'Hello,';
+    $name = 'Hiro';
+    $world = '‘s World!'
+    echo  $hello,$name,$world;
+    
+    //4問目
+    $tech_boost = 'tech';
+    $boost = ' boost';
+    echo $tech_boost,$boost;
+    
+    //5問目
+    $calendar_2018 = array[
+  "January" => "1月",
+  "February" => "2月",
+  "March" => "3月",
+  "April" => "4月",
+  "May" => "5月",
+  "June" = "6月",
+  "July" => "7月",
+  "August" => "8月",
+  "September" => "9月",
+  "October" => "10月",
+  "November" => "11月",
+  "December" => "12月"
+];
+
+echo array[11];
